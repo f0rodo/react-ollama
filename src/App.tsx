8107fe862db466fd4bda830d0045388818433ce3
Kartik Thakore <kartik@thakore.ai>
2023-08-23 12:24:10 -0700

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,165 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [history, setHistory] = useState<{ type: 'server' | 'user'; prompt: string; timestamp: number }[]>([]);
+  const [context, setContext] = useState([]);
+  const [prompt, setPrompt] = useState('');
+  const [system, setSystem] = useState('You are a helpful assistant.');
+  const [loading, setLoading] = useState(false);
+  const [feedback, setFeedback] = useState<{ [key: number]: 'up' | 'down' }>({});
+  const [editableIndex, setEditableIndex] = useState<number | null>(null);
+  const [editableText, setEditableText] = useState('');
+
+  const handleCancel = () => {
+    if (editableIndex !== null) {
+      setEditableText(history[editableIndex]?.prompt || '');
+      setEditableIndex(null);
+    }
+  };
+
+  const handleFeedback = (index: number, type: 'up' | 'down') => {
+    setFeedback({
+      ...feedback,
+      [index]: type,
+    });
+
+    if (type === 'down') {
+      setEditableIndex(index);
+      setEditableText(history[index]?.prompt || '');
+    }
+  };
+
+  const handleSave = () => {
+    if (editableIndex !== null) {
+      const updatedHistory = [...history];
+      updatedHistory[editableIndex].prompt = editableText;
+      setHistory(updatedHistory);
+      setEditableIndex(null);
+    }
+  };
+
+  const sendPrompt = async () => {
+    setLoading(true);
+
+    let tempHistory = [...history, { prompt: "", type: 'server' as 'server', timestamp: Date.now() }];
+
+    setHistory(tempHistory);
+    const tempIndex = tempHistory.length - 1;
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        model: 'llama2-uncensored',
+        prompt,
+        system,
+        template: '',
+        context,
+        options: { temperature: 0.8 }
+      })
+    };
+
+    const response = await fetch('http://127.0.0.1:11434/api/generate', requestOptions);
+    const reader = response.body?.getReader();
+
+    if (reader) {
+      let serverResponse = '';
+
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          setLoading(false);
+          break;
+        }
+
+        const decodedValue = new TextDecoder('utf-8').decode(value);
+
+        try {
+          const { response, done, context } = JSON.parse(decodedValue);
+
+          if (response) {
+            serverResponse += response;
+            tempHistory[tempIndex].prompt = serverResponse;
+            setHistory([...tempHistory]);
+          }
+
+          if (done) {
+            setContext(context);
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    // if latest message is from the user, call sendPrompt
+    if (history.length > 0 && history[history.length - 1].type === 'user') {
+      sendPrompt();
+    }
+  }, [history, sendPrompt]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="history-container">
+        <div className="history">
+          {history.map((item, index) => (
+            <div key={index} className={`message ${item.type}`}>
+              <strong>{`${item.type.toUpperCase()} ${new Date(item.timestamp).toLocaleString()}`}</strong>:
+              {editableIndex === index ? null : item.prompt}
+              {editableIndex === index ? (
+                <textarea
+                  className="textarea-editable"
+                  value={editableText}
+                  onChange={(e) => setEditableText(e.target.value)}
+                />
+              ) : null}
+              {item.type === 'server' && (
+                <div className="feedback-icons">
+                   {editableIndex === index && (
+                  <>
+                    <button className="saveBtn" onClick={handleSave}>Save</button>
+                    <button className="cancelBtn" onClick={handleCancel}>Cancel</button>
+                  </>
+                )}
+                  <span><button className={`${feedback[index] === 'up' ? 'selected' :''}`} onClick={() => handleFeedback(index, 'up')}>👍</button>
+                    <button className={`${feedback[index] === 'down' ? 'selected' :''}`} onClick={() => handleFeedback(index, 'down')}>👎</button>
+                  </span>
+                 
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="input-area">
+        <textarea
+          className="textarea"
+          placeholder="Enter your prompt"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        ></textarea>
+
+        <textarea
+          className="textarea"
+          placeholder="System prompt (optional)"
+          value={system}
+          onChange={(e) => setSystem(e.target.value)}
+        ></textarea>
+
+        <button
+          className={`send-button ${loading ? 'disabled' : ''}`}
+          disabled={loading}
+          onClick={async () => {
+            setHistory(prevHistory => [...prevHistory, { prompt, type: 'user', timestamp: Date.now() }])
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Send
+        </button>
+      </div>
     </div>
   );
 }
